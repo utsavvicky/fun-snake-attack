f3acf716007dad2f37dbe6df9e07c0c58ba37cf0
Sachin <skad5455[at]gmail[dot]com>
2016-01-14 20:13:11 +0530

    updated gameover menu and gameover in two player mode, gui changes, minor bug fixes

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame,random,sys
+import pygame,random,sys,time
 from pygame.locals import *
 
 pygame.init()
@@ -12,14 +12,15 @@ scr_height = 480
 screen = pygame.display.set_mode((scr_width,scr_height))
 
 # initial state of the snake
-n_blocks = 1
-n_blocks1 = 1
-origin = [40,40]
-origin1 = [100,100]
+n_blocks = 5
+n_blocks1 = 5
+origin = [50,50]
+origin1 = [scr_width-50,50]
 eaten = 1
 eaten1 = 1
 score = 0
 score1 = 0
+t = -1
 
 class pause_scr_item(pygame.font.Font):
         def __init__(self, text, font=None, font_size=48, font_color=(255, 255, 255), (pos_x, pos_y)=(0, 0)):
@@ -52,7 +53,7 @@ class pause_scr_item(pygame.font.Font):
 
 class snake_block:
 	'Defines the snake'
-	color = (255,255,255)
+	color = [255,255,255]
 	width = 5
 	height = 5
 	x = 0
@@ -71,9 +72,10 @@ snake=[]
 snake1=[]
 food = food_block()
 
+
 # initializing for a new game
 def init():
-	global snake, food, eaten, score, FPS, n_blocks, origin, snake1, eaten1, score1, n_blocks1, origin1
+	global t, snake, food, eaten, score, FPS, n_blocks, origin, snake1, eaten1, score1, n_blocks1, origin1
 
 	snake = [snake_block() for _ in xrange(200)]
 	
@@ -81,28 +83,25 @@ def init():
 	
 	food = food_block()
 
-	n_blocks = 1
-	n_blocks1 = 1
+	n_blocks = 5
+	n_blocks1 = 5
 	score = 0
 	score1 = 0
-
+	t = -1
 	origin[0] += n_blocks*5
 
 	for i in xrange(n_blocks):
 		snake[i].x = origin[0] - (snake[i].width*i)
 		snake[i].y = origin[1]
+		snake[i].color = [0,250,0]
 
 	origin1[0] += n_blocks1*5
 
 	for i in xrange(n_blocks1):
 		snake1[i].x = origin1[0] - (snake1[i].width*i)
 		snake1[i].y = origin1[1]
-
-	for _ in xrange(200):
-                snake[i].color=(0,250,0)
-                snake1[i].color=(0,0,200)
-
-
+		snake1[i].color = [0,0,220]
+		
 	place_food()
 	eaten = 0
 	eaten1 = 0
@@ -158,26 +157,43 @@ def move_down(snake,cur_dir,n_blocks):
                 snake[0].y = (snake[0].y+snake[0].height)%(scr_height+5)
 	else:	move_up(snake, cur_dir,n_blocks)
 
-def game_over(snake,n_blocks):
+def game_over(snake,n_blocks,mode):
+	global t
 	if n_blocks <=2 :	return
-	for i in xrange(1,n_blocks):
-		if snake[0].x == snake[i].x and snake[0].y == snake[i].y:
-			display_game_over_screen()
-			
+	
+	if mode == "multi":
+		for i in xrange(1,n_blocks):
+			if snake[0].x == snake[i].x and snake[0].y == snake[i].y:
+				t = FPS*10
+				snake[0].x = -1
+				for _ in xrange(n_blocks):
+					snake[_].color = (0,0,0)
+				break
 
-def display_game_over_screen():
+	else:
+		for i in xrange(1,n_blocks):
+			if snake[0].x == snake[i].x and snake[0].y == snake[i].y:
+				display_game_over_screen(mode)
+		
+
+def display_game_over_screen(mode):
 	gover_font = pygame.font.SysFont(None, 48)
 	other_font = pygame.font.SysFont(None, 40)
 
 	gameover = gover_font.render("Gameover!", True, (255,255,255))
-	scored = other_font.render("Score: %d"%score, True, (255,255,255))
+	
+	if mode == "multi":
+		scored2 = other_font.render("Score: %d"%score1, True, (0,0,255))
+	        scored2_pos = [(scr_width/2) - (scored2.get_rect().width / 2), (scr_height / 2) + 4]
+	
+	scored= other_font.render("Score: %d"%score, True,(0,255,0))
 	play_again = other_font.render("Play Again?", True, (255,255,255))
 	quit = other_font.render("Quit", True, (255,255,255))
 
 	gameover_pos = [(scr_width / 2) - (gameover.get_rect().width / 2), (scr_height / 2) - (2*gameover.get_rect().height)]
 	scored_pos = [(scr_width / 2) - (scored.get_rect().width / 2), (scr_height / 2) - (scored.get_rect().height) + 2]
-	play_again_pos = [(scr_width / 2) - (play_again.get_rect().width / 2), (scr_height / 2) + 4]
-	quit_pos = [(scr_width / 2) - (quit.get_rect().width / 2), (scr_height / 2) + (quit.get_rect().height) + 6]
+	play_again_pos = [(scr_width / 2) - (play_again.get_rect().width / 2), (scr_height / 2) + (play_again.get_rect().height) + 6]
+	quit_pos = [(scr_width / 2) - (quit.get_rect().width / 2), (scr_height / 2) + (2*quit.get_rect().height) + 8]
 
 	loop = True
 
@@ -190,7 +206,7 @@ def display_game_over_screen():
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				pos = pygame.mouse.get_pos()
 
-				if pos[0] >= play_again_pos[0] and pos[0] <= play_again_pos[0] + play_again.get_rect().width and pos[1] >= play_again_pos[1] and pos[1] <= play_again_pos[1] + play_again.get_rect().height:	run()
+				if pos[0] >= play_again_pos[0] and pos[0] <= play_again_pos[0] + play_again.get_rect().width and pos[1] >= play_again_pos[1] and pos[1] <= play_again_pos[1] + play_again.get_rect().height:	run(mode)
 
 				if pos[0] >= quit_pos[0] and pos[0] <= quit_pos[0] + quit.get_rect().width and pos[1] >= quit_pos[1] and pos[1] <= quit_pos[1] + quit.get_rect().height:
 					pygame.quit()
@@ -200,68 +216,52 @@ def display_game_over_screen():
 
 		screen.blit(gameover, gameover_pos)
 		screen.blit(scored, scored_pos)
+	
+		if mode=="multi":
+			screen.blit(scored2, scored2_pos)
+	
 		screen.blit(play_again, play_again_pos)
 		screen.blit(quit, quit_pos)
 
 		pygame.display.flip()
 
-def score_toolbar():
+def score_toolbar(mode):
 	t_height = 40
 	t_width = scr_width
-
+	global t
 	font = pygame.font.SysFont(None, t_height/2)
-	score_txt = font.render("S1: %d"%score, True, (255,255,255))
+	score_txt = font.render("S1: %d"%score, True, (0,255,0))
 	
 	score_txt_pos = [10, t_height/2 - score_txt.get_rect().height/2]
-
+	
 	screen.blit(score_txt, score_txt_pos)
 
-        score1_txt = font.render("S2: %d"%score1, True, (255,255,255))
+	if mode=="multi":
+		score1_txt = font.render("S2: %d"%score1, True, (0,0,255))
 
-        score1_txt_pos = [t_width-50, t_height/2 - score1_txt.get_rect().height/2]
+		score1_txt_pos = [t_width-50, t_height/2 - score1_txt.get_rect().height/2]
 
-        screen.blit(score1_txt, score1_txt_pos)
+		time_txt = font.render("Time Left: %0.2f"%(t/FPS), True,(255,255,255))
 
-def draw_single():
+		time_position = [(t_width/2)-time_txt.get_rect().width/2, t_height/2 - time_txt.get_rect().height/2]
 
-        global snake, snake1, eaten, n_blocks, FPS, score, eaten1, n_blocks1, score1
+		screen.blit(score1_txt, score1_txt_pos)
+		if t >= 0 :
+			screen.blit(time_txt, time_position)
 
-        BLACK = [0,0,0]
 
-        screen.fill(BLACK)
-
-        score_toolbar()
-
-        for i in xrange(n_blocks):
-                pygame.draw.rect(screen,snake[i].color,(((snake[i].x%(scr_width+5)),(snake[i].y%(scr_height+5))),(snake[i].width,snake[i].height)))
-        eaten = snake[0].x == food.x and snake[0].y == food.y
-
-        if eaten:
-                place_food()
-                eaten = 0
-                eaten1=0
-                n_blocks += 1
-                #print n_blocks
-                snake[n_blocks-1].x=snake[n_blocks-2].x                 # adding new block when food is consumed at the last block position
-                snake[n_blocks-1].y=snake[n_blocks-2].y
-                FPS += 0.5                                              # increasing speed after every food consumption
-                score += 10
-
-	pygame.draw.rect(screen,food.color,((food.x, food.y), (food.width, food.height)))
-
-        pygame.display.update()
-
-        fpsClock.tick(FPS)
-
-def draw_multi():
+def draw(mode):
 
 	global snake, snake1, eaten, n_blocks, FPS, score, eaten1, n_blocks1, score1
 
 	BLACK = [0,0,0]
+	WHITE = [255,255,255]
+	GREEN = [0,250,0]
+	BLUE = [0,0,220]
 
 	screen.fill(BLACK)
 	
-	score_toolbar()
+	score_toolbar(mode)
 
 	for i in xrange(n_blocks):
 		pygame.draw.rect(screen,snake[i].color,(((snake[i].x%(scr_width+5)),(snake[i].y%(scr_height+5))),(snake[i].width,snake[i].height)))
@@ -272,26 +272,27 @@ def draw_multi():
 		eaten = 0
 		eaten1=0
 		n_blocks += 1
-		#print n_blocks
 		snake[n_blocks-1].x=snake[n_blocks-2].x			# adding new block when food is consumed at the last block position
 		snake[n_blocks-1].y=snake[n_blocks-2].y
-		FPS += 0.5						# increasing speed after every food consumption
+		snake[n_blocks-1].color = GREEN
+		FPS += 0.75						# increasing speed after every food consumption
 		score += 10
 
-	for i in xrange(n_blocks1):
-                pygame.draw.rect(screen,snake1[i].color,(((snake1[i].x%(scr_width+5)),(snake1[i].y%(scr_height+5))),(snake1[i].width,snake1[i].height)))
-        eaten1 = snake1[0].x == food.x and snake1[0].y == food.y
+	if mode=="multi":
+		for i in xrange(n_blocks1):
+			pygame.draw.rect(screen,snake1[i].color,(((snake1[i].x%(scr_width+5)),(snake1[i].y%(scr_height+5))),(snake1[i].width,snake1[i].height)))
+		eaten1 = snake1[0].x == food.x and snake1[0].y == food.y
 
-        if eaten1:
-                place_food()
-                eaten1 = 0
-		eaten=0
-                n_blocks1 += 1
-                #print "s1   "+str(n_blocks1)+"snake  "+str(n_blocks)
-                snake1[n_blocks1-1].x=snake1[n_blocks1-2].x                 # adding new block when food is consumed at the last block position
-                snake1[n_blocks1-1].y=snake1[n_blocks1-2].y
-                FPS += 0.5                                              # increasing speed after every food consumption
-                score1 += 10
+		if eaten1:
+			place_food()
+			eaten1 = 0
+			eaten=0
+			n_blocks1 += 1
+			snake1[n_blocks1-1].x=snake1[n_blocks1-2].x                 # adding new block when food is consumed at the last block position
+			snake1[n_blocks1-1].y=snake1[n_blocks1-2].y
+			snake1[n_blocks1-1].color = BLUE
+			FPS += 0.75                                              # increasing speed after every food consumption
+			score1 += 10
 
  
 
@@ -373,18 +374,24 @@ def run(mode):
 	cur_dir1 = "RIGHT"
         prev_dir1 = ""
 
+	global t
+
 	main_loop = True
 
 	init()
 
 	while main_loop:
-		if mode == "multi":
-			draw_multi()
-		if mode == "single":
-			draw_single()
-		#draw(snake1, eaten1, n_blocks1, score1, FPS)
-		game_over(snake,n_blocks)
-		game_over(snake1,n_blocks1)
+		
+		draw(mode)
+		
+		if t==-1:
+			game_over(snake,n_blocks,mode)
+			game_over(snake1,n_blocks1,mode)
+
+		if t > 0:
+			t-=1
+		elif t!=-1:
+			display_game_over_screen(mode)
 
 		for event in pygame.event.get():
 			if event.type ==pygame.QUIT:
